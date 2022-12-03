10204e3e40ce68458137f2c9dd61dd3492407bc6
m-hamdy1572 <mh3908334@gmail.com>
2022-12-03 14:57:00 +0200

    sd

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ app.use(expressRateLimit({
 app.use( (req, res, next)=> {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH, OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Content-Type, api_key, Authorization, application/json');
   next();
 });
 
