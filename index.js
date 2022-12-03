b66849185e8237c4d06bd4cdb59e6c83cf0aeec8
m-hamdy1572 <mh3908334@gmail.com>
2022-12-03 14:45:21 +0200

    sd

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ app.use(expressRateLimit({
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 }))
 
+app.use( (req, res, next)=> {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
