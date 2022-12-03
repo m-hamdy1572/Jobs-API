9295da87576f3ede193d9f1a3756ce25fc24f961
m-hamdy1572 <mh3908334@gmail.com>
2022-12-03 12:02:41 +0200

    SAAD

--- index.js
+++ index.js
@@ -40,18 +40,19 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xssClean());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-// const options = {
-//   customCssUrl: '/public/swagger-ui.css'
-// };
+
+const options = {
+  customCssUrl: '/public/swagger-ui.css'
+};
 
 app.get('/', (req, res) => {
   res.redirect('/api-docs');
   //res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
 });
+app.use('/public', express.static(path.join(__dirname, 'public')));a
 app.use('/api-docs', swaggerUI.serve);
-app.get('/api-docs', swaggerUI.setup(swaggerDocument));
+app.get('/api-docs', swaggerUI.setup(swaggerDocument,options));
 
 // routes
 app.use('/api/v1/auth', authRouter);
