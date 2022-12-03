632f50a6aa77b7affdd0d28da2b29686a40c28b8
m-hamdy1572 <mh3908334@gmail.com>
2022-12-03 13:15:29 +0200

    SAAD

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ app.use(xssClean());
 
 const options = {
   customCssUrl: '/public/swagger-ui.css',
-  customHRMLUrl:'/public/index.html'
 };
 
 app.get('/', (req, res) => {
   res.redirect('/api-docs');
+  res.setHeader('content-type', 'text/css');
   //res.send('<h1>Jobs API</h1> <a href="/api-docs">Documentation</a>');
 });
 app.use('/public', express.static(path.join(__dirname, 'public')));
