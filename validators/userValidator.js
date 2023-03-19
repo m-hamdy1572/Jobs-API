2f8d53d5d0d696f863bc45a28a3f3cbc7c13f2fd
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-03-20 00:09:04 +0200

    add validation layeer and security

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,12 +1,19 @@
 const { check } = require('express-validator');
 const { validatorMiddleWare } = require('../middleware/validatorMiddleWare');
+const User = require('../models/Users');
 exports.signupValidator = [
   check('name')
     .notEmpty().withMessage('Name must be required')
     .isLength({ min: 3, max: 50 }).withMessage('Name must be between 3 t0 50'),
   check('email')
     .notEmpty().withMessage('E-mail required')
-    .isEmail().withMessage('E-mail invalid'),
+    .isEmail().withMessage('E-mail invalid').custom(async (val) => {
+      const user = await User.findOne({ Email: val });
+      if (user)
+        throw new BadRequest(`This E-mail is already  used`);
+      return true;
+    }
+  ),
   check('password')
     .notEmpty().withMessage('Password required')
     .isLength({ min: 6 }).withMessage('Password greater than 6 '),
